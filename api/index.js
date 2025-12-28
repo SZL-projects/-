7027c33323ba8491e77b0251a7711cb4245ccfec
שלמה זלמן לרנר <slerner@motorcycle-unit.local>
2025-12-28 03:23:08 +0200

    תיקון: שינוי ייצוא API לפורמט Vercel Functions

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 // Vercel Serverless Function
 const app = require('../backend/server-firebase');
 
-module.exports = app;
+// Export as Vercel function
+module.exports = (req, res) => {
+  return app(req, res);
+};
